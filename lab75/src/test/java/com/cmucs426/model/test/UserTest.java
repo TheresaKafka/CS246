98c1104de96e01101fa83e4948a14eba3c66cbcc
TheresaKafka <gmhuy2004@gmail.com>
2025-04-13 17:28:55 +0700

    TestUser

---
+++ lab75/src/test/java/com/cmucs426/model/test/UserTest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cmucs426.model.test;
+
+/**
+ *
+ * @author ACER
+ */
+import com.mycompany.lab75.User;
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+    public void testUserConstructor() {
+        User user = new User("user1", "user1@cmucs246.com");
+        assertEquals("user1", user.getEmail());
+    }
+
+    public void testSetEmail() {
+        User user = new User("user1", "user1@cmucs246.com");
+        user.setEmail("newemail@cmucs246.com");
+        assertEquals("newemail@cmucs246.com", user.getEmail());
+    }
+
+    public void testUpdateEmail() {
+        User user = new User("user1", "user1@cmucs246.com");
+        user.updateEmail("updatedemail@cmucs246.com");
+        assertEquals("updatedemail@cmucs246.com", user.getEmail());
+
+    }
+}
